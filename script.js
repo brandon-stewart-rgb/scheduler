bc906fa7061d42c8e3a6681e546e9f464257f2e8
brandon-stewart-rgb <brandon@pixeltree.us>
2021-07-16 15:38:29 -0600

    Time Classes

--- script.js
+++ script.js
@@ -31,45 +31,91 @@
       document.getElementById('current-day').innerText = s;
 
 
-      // time 
+     //  greater than > or less than <
 
-   //    $(function() {
-   //       var hour = (new Date).getHours() -10;
+      $(function() {
+         var hour = (new Date).getHours() -12;
      
-   //       $('.select').removeClass('select');
+         $('.select').removeClass('select');
          
-   //       if (hour < 7) {
-   //           $('#btn_1').addClass('select');
-   //       } else if (hour < 11) {
-   //           $('#btn_2').addClass('select');
-   //       } else if (hour < 16) {
-   //           $('#btn_3').addClass('select');
-   //       }
+         if (hour > 8) {
+             $('#eight').addClass('red');
+         } else if (hour < 11) {
+            $('#eight').addClass('blue');
+         } else if (hour < 16) {
+             $('#btn_3').addClass('select');
+         }
+
+         console.log(hour)
+         // perhaps this works?
      
-   //   })
+     });
+
+
+     $(document).ready(function() {
+      $("div.all").hide();
+      var date = new Date();
+      var d = date.getDay();
+      var h = date.getHours();
+      console.log('searching for ' + d + ' and ' + h);
+      $("div.all").each(function() {
+          if ( $(this).hasClass("d" + d) && $(this).hasClass("h"+h) ) {
+              $(this).show();
+          }
+      });
+  });
 
    //https://stackoverflow.com/questions/15083548/convert-12-hour-hhmm-am-pm-to-24-hour-hhmm/22497471
 
-   const convertTime12to24 = (time12h) => {
-      const [time, modifier] = time12h.split(' ');
+   // const convertTime12to24 = (time12h) => {
+   //    const [time, modifier] = time12h.split(' ');
     
-      let [hours, minutes] = time.split(':');
+   //    let [hours, minutes] = time.split(':');
     
-      if (hours === '12') {
-        hours = '00';
-      }
+   //    if (hours === '12') {
+   //      hours = '00';
+        
+   //    }
     
-      if (modifier === 'PM') {
-        hours = parseInt(hours, 10) + 12;
-      }
+   //    if (modifier === 'PM') {
+   //      hours = parseInt(hours, 10) + 12;
+   //    }
     
-      return `${hours}:${minutes}`;
-    }
+  
+   //    return `${hours}:${minutes}`;
+   //  }
     
-    console.log(convertTime12to24('09:00 AM'));
-    console.log(convertTime12to24('05:06 PM'));
-    console.log(convertTime12to24('12:00 PM'));
-    console.log(convertTime12to24('12:00 AM'));
+   //  console.log(convertTime12to24('09:00 AM'));
+   //  console.log(convertTime12to24('05:06 PM'));
+   //  console.log(convertTime12to24('12:00 PM'));
+   //  console.log(convertTime12to24('12:00 AM'));
+   //  console.log(convertTime12to24('03:00 pm'));
+   //  function myFunction($param) {
+   //    $param.hide();  // or whatever you want to do
+
+     
+      
+   // }
+   
+
+ //  greater than > or less than <
+
+//  if (hours > '04:00 PM') {
+//    $('#eight').addClass('red');
+// }
+
+
+//    $.fn.myTime = function () {
+//       if (time12> '09:00 AM') {
+//          $('#eight').addClass('red');
+//          console.log(convertTime12to24
+//             )
+//      } else if (hour < 11) {
+//          $('#btn_2').addClass('select');
+//      } else if (hour < 16) {
+//          $('#btn_3').addClass('select');
+//      }
+//   };
 
       // Declares a 'scheduleList' variable that holds the parsed 'schedule' items retrieved from 'localStorage'
       // If there is nothing in 'localStorage', sets the 'scheduleList' to an empty array
@@ -77,7 +123,7 @@
 
 
       // when form is clicked turns <p> into <textarea>
-      $(".row").on("click", "p", function () {
+      $(".row").on("click", "span", function () {
          var list= $(this)
             .text()
             .trim();
@@ -100,7 +146,7 @@
             var userInput = $(this).val().trim();
             
             // recreate the p element
-            var scheduleParagraphEl = $("<p>").addClass("form-control form-control-lg bleh").text(userInput);
+            var scheduleParagraphEl = $("<span>").addClass("form-control form-control-lg bleh").text(userInput);
                
                
             // replace textarea with paragraph
