12b5b2e853bc2c3baf25128e29e29639407a374b
brandon-stewart-rgb <brandon@pixeltree.us>
2021-07-19 16:16:05 -0600

    Code Rework

--- script.js
+++ script.js
@@ -1,192 +1,69 @@
-
-
-
-// get hours etc https://linuxhint.com/how_to_get_current_date_and_time_in_javascript/
-
-// One day (24 hours) is 86 400 000 milliseconds.
-
-
-  
-   // empty array variable 
-   var tasks = [];
-   var taskIdCounter = 0;
-
-
-// update class via time of day
-
-//  https://stackoverflow.com/questions/27348048/change-div-depending-on-hour-of-day-with-jquery
-
-
-
-      function join(t, a, s) {
-         function format(m) {
-            let f = new Intl.DateTimeFormat('en', m);
-            return f.format(t);
-         }
-         return a.map(format).join(s);
-      }
-      
-      let a = [{month: 'long'}, {day: 'numeric'},  {year: 'numeric'}];
-      let s = join(new Date, a, ' | ');
-      document.getElementById('current-day').innerText = s;
-
-
-     //  greater than > or less than <
-
-      $(function() {
-         var hour = (new Date).getHours() -12;
-     
-         $('.select').removeClass('select');
-         
-         if (hour > 8) {
-             $('#eight').addClass('red');
-         } else if (hour < 11) {
-            $('#eight').addClass('blue');
-         } else if (hour < 16) {
-             $('#btn_3').addClass('select');
-         }
-
-         console.log(hour)
-         // perhaps this works?
-     
-     });
-
-
-     $(document).ready(function() {
-      $("div.all").hide();
-      var date = new Date();
-      var d = date.getDay();
-      var h = date.getHours();
-      console.log('searching for ' + d + ' and ' + h);
-      $("div.all").each(function() {
-          if ( $(this).hasClass("d" + d) && $(this).hasClass("h"+h) ) {
-              $(this).show();
-          }
-      });
-  });
-
-   //https://stackoverflow.com/questions/15083548/convert-12-hour-hhmm-am-pm-to-24-hour-hhmm/22497471
-
-   // const convertTime12to24 = (time12h) => {
-   //    const [time, modifier] = time12h.split(' ');
-    
-   //    let [hours, minutes] = time.split(':');
-    
-   //    if (hours === '12') {
-   //      hours = '00';
-        
-   //    }
-    
-   //    if (modifier === 'PM') {
-   //      hours = parseInt(hours, 10) + 12;
-   //    }
-    
-  
-   //    return `${hours}:${minutes}`;
-   //  }
-    
-   //  console.log(convertTime12to24('09:00 AM'));
-   //  console.log(convertTime12to24('05:06 PM'));
-   //  console.log(convertTime12to24('12:00 PM'));
-   //  console.log(convertTime12to24('12:00 AM'));
-   //  console.log(convertTime12to24('03:00 pm'));
-   //  function myFunction($param) {
-   //    $param.hide();  // or whatever you want to do
-
-     
-      
-   // }
+$(document).ready(function () {
    
 
- //  greater than > or less than <
+   //display current day & time.
+   //  moment.js
+   $("#current-day").text(moment().format("MMMM Do, YYYY")); 
+   $("#current-hour").text(moment().format("h:mm ")); 
 
-//  if (hours > '04:00 PM') {
-//    $('#eight').addClass('red');
-// }
+     var schedule = [];
 
+        $(".btn").on("click", function(){
+            var userInput = $(this).siblings(".description").val();
+            var timeId = $(this).parent().attr("id");
 
-//    $.fn.myTime = function () {
-//       if (time12> '09:00 AM') {
-//          $('#eight').addClass('red');
-//          console.log(convertTime12to24
-//             )
-//      } else if (hour < 11) {
-//          $('#btn_2').addClass('select');
-//      } else if (hour < 16) {
-//          $('#btn_3').addClass('select');
-//      }
-//   };
+            schedule.push( {
+                description: userInput, 
+                time: timeId, 
+            });
 
-      // Declares a 'scheduleList' variable that holds the parsed 'schedule' items retrieved from 'localStorage'
-      // If there is nothing in 'localStorage', sets the 'scheduleList' to an empty array
-      var item = JSON.parse(localStorage.getItem('text')) || [];
-
-
-      // when form is clicked turns <p> into <textarea>
-      $(".row").on("click", "span", function () {
-         var list= $(this)
-            .text()
-            .trim();
-
-         var textInput = $("<textarea>")
-            .addClass("form-control form-control-lg foo")
-            .val(list);
-
-         $(this).replaceWith(textInput);
-         textInput.trigger("focus");
-   
-      });
+            localStorage.setItem("schedule", JSON.stringify(schedule));
+        })
 
 
 
+var storedSchedule = JSON.parse(localStorage.getItem("schedule"));
 
-         //when user clicks outside the textarea turns <textarea> back to <p> with added text
-         $(".row").on("blur", "textarea", function () {
-            // get the textarea current value/text
-            var userInput = $(this).val().trim();
-            
-            // recreate the p element
-            var scheduleParagraphEl = $("<span>").addClass("form-control form-control-lg bleh").text(userInput);
-               
-               
-            // replace textarea with paragraph
-            $(this).replaceWith(scheduleParagraphEl);
+if (storedSchedule !== null) {
+    schedule = storedSchedule;
+}
+ 
 
-
-            // add task id as a custom attribute incremented from 0 by taskIdCounter
-            scheduleParagraphEl.attr("data-task-id", taskIdCounter);
+// loop over schedule
+for (let i = 0; i < schedule.length; i++) {
+    var userDescription = schedule[i].description;
+    $("#" + schedule[i].time).children(".description").text(userDescription);
+  }
+    
 
 
+   function hourTracker() {
+       //get current number of hours.
+       // moment.js
+       var currentHour = moment().hour(); 
 
-            var x = [userInput, scheduleParagraphEl];
-
-            tasks.push(x);
-
-
-            // scheduleParagraphEl.id = taskIdCounter;
-            // // adds any content between the parentheses to the end of the specified and empty array of 'tasks'
-            // tasks.push(scheduleParagraphEl);
-      
-
-            // increase task counter for next unique id
-            taskIdCounter++;
-
-            // add text to local storage
-            // localStorage.setItem('item', JSON.stringify(text));
+       // loop over time blocks
+       $(".time-block").each(function () {
+           var blockHour = parseInt($(this).attr("id").split("hour")[1]);
            
-         });
 
-
-         
-
-
-
-
-// var taskId = 1; 
-
-// var divEl = document.createElement("div");
-// divEl.className = "task-item";
-// divEl.setAttribute("data-id", taskId);
-
-
-
+           //check if we've moved past this time, click into css/html given classes of past, present, or future
+           if (blockHour < currentHour) {
+               $(this).addClass("past");
+               $(this).removeClass("future");
+               $(this).removeClass("present");
+           }
+           else if (blockHour === currentHour) {
+               $(this).removeClass("past");
+               $(this).addClass("present");
+               $(this).removeClass("future");
+           }
+           else {
+               $(this).removeClass("present");
+               $(this).removeClass("past");
+               $(this).addClass("future");
+           }
+       })
+   }
+   hourTracker(); 
+});
