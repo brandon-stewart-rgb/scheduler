e026f3db24f594b65be4da011f228703c0116052
brandon-stewart-rgb <brandon@pixeltree.us>
2021-07-20 21:21:28 -0600

    Final Push

--- script.js
+++ script.js
@@ -1,53 +1,76 @@
 $(document).ready(function () {
    
-
-   //display current day & time.
+   //display current day & time
    //  moment.js
    $("#current-day").text(moment().format("MMMM Do, YYYY")); 
    $("#current-hour").text(moment().format("h:mm ")); 
 
-     var schedule = [];
 
-        $(".btn").on("click", function(){
-            var userInput = $(this).siblings(".description").val();
-            var timeId = $(this).parent().attr("id");
 
-            schedule.push( {
-                description: userInput, 
-                time: timeId, 
-            });
-
-            localStorage.setItem("schedule", JSON.stringify(schedule));
-        })
+    // empty array to store arrays
+   var schedule = [];
 
 
 
-var storedSchedule = JSON.parse(localStorage.getItem("schedule"));
 
-if (storedSchedule !== null) {
-    schedule = storedSchedule;
-}
- 
+    $(".btn").on("click", function(){
 
-// loop over schedule
-for (let i = 0; i < schedule.length; i++) {
-    var userDescription = schedule[i].description;
-    $("#" + schedule[i].time).children(".description").text(userDescription);
-  }
+        // grab the value from the text area and place that value in an object
+        var userInput = $(this).siblings(".description").val();
+
+        // get the div id from closest parent of div on click
+        var timeId = $(this).parent().attr("id");
+
+        //combine variables above and push into local storage as 'schedule'
+        schedule.push( {
+            description: userInput, 
+            time: timeId, 
+        });
+
+        // set local storage and stringify
+        localStorage.setItem("schedule", JSON.stringify(schedule));
+
+    });
+
+
+
+    // retrieve items from local storage and store in object, storedSchedule
+    var storedSchedule = JSON.parse(localStorage.getItem("schedule"));
+
+
+    // if storedSchedule is null, let storedSchedule become schedule
+    if (storedSchedule !== null) {
+        schedule = storedSchedule;
+    };
     
 
 
-   function hourTracker() {
-       //get current number of hours.
-       // moment.js
-       var currentHour = moment().hour(); 
 
-       // loop over time blocks
-       $(".time-block").each(function () {
+    // loop over entire schedule
+    for (let i = 0; i < schedule.length; i++) {
+
+        var userDescription = schedule[i].description;
+    
+        // check id and place in child or .description
+        $("#" + schedule[i].time).children(".description").text(userDescription);
+    };
+    
+
+
+
+
+    function hourTracker() {
+      
+        // moment.js
+        var currentHour = moment().hour(); 
+
+        // loop over time blocks
+         $(".time-block").each(function () {
+
+            // variable for getting hour ids from time blocks to check for current hour and update classes
            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
            
-
-           //check if we've moved past this time, click into css/html given classes of past, present, or future
+            //see if we've moved past a certain hour and the update classes
            if (blockHour < currentHour) {
                $(this).addClass("past");
                $(this).removeClass("future");
@@ -63,7 +86,20 @@ for (let i = 0; i < schedule.length; i++) {
                $(this).removeClass("past");
                $(this).addClass("future");
            }
-       })
-   }
-   hourTracker(); 
+        });
+    };
+
+
+    // run the hour tracker again
+    hourTracker(); 
+
+
+    // clear the local storage localStorage.clear(); via button to clear the calendar and reload the page location.reload()
+    $(".clear-btn").on("click", function(){
+        localStorage.clear();
+        location.reload();
+    });
+ 
+
+
 });
